fdeed590cc38c41dca3a524a19139dbf17809b99
Thomaz Guadagnini <59666076+ThomazGR@users.noreply.github.com>
2022-04-23 16:15:19 -0300

    Gnome customization, GH CLI login through token, Flatpaks, micro editor

--- pop_os_runner.sh
+++ pop_os_runner.sh
@@ -21,13 +21,6 @@ sudo apt install apt-transport-https
 # Update and instal some needed packages
 sudo apt update && sudo apt upgrade -y
 
-# Removes nosnap.pref if exists (usually in Linux Mint)
-SNAP_PREF=/etc/apt/preferences.d/nosnap.pref
-if [ -f "$SNAP_PREF" ]; then
-	sudo mv /etc/apt/preferences.d/nosnap.pref $HOME/Documents/nosnap.backup
-	sudo apt update
-fi
-
 # Check if flatpak is installed, if not install
 if ! [ -x "$(command -v flatpak)" ]; then
 	echo 'ERROR: Flatpak is not installed.' >&2
@@ -40,50 +33,86 @@ else
 fi
 
 # Install some softwares via apt
-sudo apt install git fonts-powerline wget curl lm-sensors -y
-sudo apt install code vlc -y
+sudo apt install git wget curl code fonts-powerline lm-sensors -y
+
+# Install GH CLI
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt update && sudo apt install gh -y
 
 #-------------------------
-# Download YoutubeMusic Appimage
+# Download YoutubeMusic
 #-------------------------
 RELEASE_VERSION_YTM=$(wget -qO - "https://api.github.com/repos/th-ch/youtube-music/releases/latest" | grep -Po '"tag_name": ?"v\K.*?(?=")')
 wget -O $HOME/Downloads/YTM.deb "https://github.com/th-ch/youtube-music/releases/download/v${RELEASE_VERSION_YTM}/youtube-music_${RELEASE_VERSION_YTM}_amd64.deb"
 sudo dpkg -i $HOME/Downloads/YTM.deb
 
 #-------------------------
+# Download Micro editor
+#-------------------------
+RELEASE_VERSION_MICRO=$(wget -qO - "https://api.github.com/repos/zyedidia/micro/releases/latest" | grep -Po '"tag_name": ?"v\K.*?(?=")')
+wget -O $HOME/Downloads/micro.deb "https://github.com/zyedidia/micro/releases/download/v${RELEASE_VERSION_MICRO}/micro-${RELEASE_VERSION_MICRO}-amd64.deb"
+sudo dpkg -i $HOME/Downloads/micro.deb
+
+#-------------------------
 # Install OnlyOffice
 #-------------------------
-flatpak install org.onlyoffice.desktopeditors
+flatpak install flathub org.onlyoffice.desktopeditors -y
 
 #-------------------------
 # Install Joplin
 #-------------------------
-flatpak install net.cozic.joplin_desktop
+flatpak install flathub net.cozic.joplin_desktop -y
 
 #-------------------------
 # Install Bitwarden
 #-------------------------
-flatpak install com.bitwarden.desktop
+flatpak install flathub com.bitwarden.desktop -y
 
 #-------------------------
 # Install Ext Manager
 #-------------------------
-flatpak install com.mattjakeman.ExtensionManager
+flatpak install flathub com.mattjakeman.ExtensionManager -y
 
 #-------------------------
 # Install GH Desktop
 #-------------------------
-flatpak install flathub io.github.shiftey.Desktop
+flatpak install flathub io.github.shiftey.Desktop -y
 
 #-------------------------
 # Install Thunderbird
 #-------------------------
-flatpak install flathub org.mozilla.Thunderbird
+flatpak install flathub org.mozilla.Thunderbird -y
 
 #-------------------------
 # Install Gnome Boxes
 #-------------------------
-flatpak install org.gnome.Boxes
+flatpak install flathub org.gnome.Boxes -y
+
+#-------------------------
+# Install Jitsi Meet
+#-------------------------
+flatpak install flathub org.jitsi.jitsi-meet -y
+
+#-------------------------
+# Install Discord
+#-------------------------
+flatpak install flathub com.discordapp.Discord -y
+
+#-------------------------
+# Install VLC
+#-------------------------
+flatpak install flathub org.videolan.VLC -y
+
+#-------------------------
+# Install GIMP
+#-------------------------
+flatpak install flathub org.gimp.GIMP -y
+
+#-------------------------
+# Install Session Desktop
+#-------------------------
+flatpak install flathub network.loki.Session -y
 
 #-------------------------
 # Install auto-cpufreq
@@ -98,43 +127,32 @@ printf "Setting git config global user name and email"
 git config --global user.name githubUserName
 git config --global user.email myEmailAddress
 
+if ! [ -z "$ghToken" ]; then
+	{
+		echo $ghToken >> .token.txt
+		gh auth login --with-token < .token.txt
+		rm -rf .token.txt
+		echo "Github logged in."
+	} || {
+		echo "Github NOT logged in. Check if everything is good or retry."
+	}
+else
+	echo "No GH Token has been passed."
+fi
+
 #-------------------------
 # Installing Miniconda
 #-------------------------
 printf "Downloading miniconda installer and installing miniconda for Linux"
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O $HOME/Downloads/miniconda.sh
 bash $HOME/Downloads/miniconda.sh -b -p $HOME/miniconda
-export PATH="$HOME/miniconda/bin:$PATH"
 
-printf "Adding conda config channels"
-conda config --add channels r
-conda config --add channels conda-forge
-conda config --add channels defaults
-conda config --add channels bioconda
-
-printf "Adding conda packages"
-conda install -c bioconda fastqc --yes
-conda install -c bioconda trim-galore --yes
-conda install -c bioconda kallisto --yes
-conda install -c bioconda picard --yes
-
-#-------------------------
-# Pre-install for R
-#-------------------------
-printf "Pre-installing R adding R project repository"
-sudo apt install --no-install-recommends software-properties-common dirmngr
-wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
-sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
-
-# Update
-sudo apt update
-
-#-------------------------
-# Install R base, dev, some libs for R
-#-------------------------
-printf "Installing r-base, r-base-dev and libs"
-sudo apt install --no-install-recommends r-base -y
-sudo apt install r-base-dev libxml2-dev libcurl4-openssl-dev libssl-dev -y
+if ! [ -x "$(command -v conda)" ]; then
+	export PATH="$HOME/miniconda/bin:$PATH"
+	conda init
+else
+	conda init
+fi
 
 #-------------------------
 # Install prompter
@@ -149,3 +167,14 @@ cp ./prompt.config $HOME/.config/gr8sh/
 # Update .bashrc
 #-------------------------
 cat tobash.conf >> $HOME/.bashrc
+
+#-------------------------
+# Customize Gnome
+#-------------------------
+sudo apt install gnome-tweaks
+
+git clone https://github.com/jmattheis/gruvbox-dark-icons-gtk ~/.icons/gruvbox-dark-icons-gtk
+
+echo "# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #"
+echo "Install the Nordic theme from here: https://github.com/EliverLara/Nordic"
+echo "# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #"
