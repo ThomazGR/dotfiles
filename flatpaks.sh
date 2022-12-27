315d64de6d3c6d9e1cc89231714095d4e6c8f032
Thomaz Guadagnini <thzgr@tuta.io>
2022-12-27 16:09:35 -0300

    separated main into flatpak file

---
+++ flatpaks.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+#-------------------------
+# Install OnlyOffice
+#-------------------------
+flatpak install --user flathub org.onlyoffice.desktopeditors -y
+
+#-------------------------
+# Install Joplin
+#-------------------------
+flatpak install --user flathub net.cozic.joplin_desktop -y
+
+#-------------------------
+# Install Bitwarden
+#-------------------------
+flatpak install --user flathub com.bitwarden.desktop -y
+
+#-------------------------
+# Install Ext Manager
+#-------------------------
+flatpak install --user flathub com.mattjakeman.ExtensionManager -y
+
+#-------------------------
+# Install Jitsi Meet
+#-------------------------
+# flatpak install --user flathub org.jitsi.jitsi-meet -y
+
+#-------------------------
+# Install VLC
+#-------------------------
+flatpak install --user flathub org.videolan.VLC -y
+
+#-------------------------
+# Install GIMP
+#-------------------------
+flatpak install --user flathub org.gimp.GIMP -y
+
+#-------------------------
+# Install Session Desktop
+#-------------------------
+# flatpak install --user flathub network.loki.Session -y
+
+#-------------------------
+# Install MEGASync
+#-------------------------
+flatpak install --user flathub nz.mega.MEGAsync -y
+
+#-------------------------
+# Install Tutanota Desktop
+#-------------------------
+flatpak install --user flathub com.tutanota.Tutanota -y
+
+#-------------------------
+# Install Gnome Auth
+#-------------------------
+flatpak install --user flathub com.belmoussaoui.Authenticator -y
+
+#-------------------------
+# Install Zotero
+#-------------------------
+flatpak install --user flathub org.zotero.Zotero -y
+
+#-------------------------
+# Install Amberol
+#-------------------------
+# flatpak install --user flathub io.bassi.Amberol -y
+
+#-------------------------
+# Install Ungoogled Chromium
+#-------------------------
+flatpak install --user flathub com.github.Eloston.UngoogledChromium -y
+
+#-------------------------
+# Install Flatseal
+#-------------------------
+flatpak install --user flathub com.github.tchx84.Flatseal -y
+
